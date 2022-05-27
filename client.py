d45864286ce6bf4f4475f03d3f58fec64b9eabfe
Xingjian <59945588+xingjianW@users.noreply.github.com>
2022-05-27 16:14:49 -0700

    登录功能1.0

--- client.py
+++ client.py
@@ -1,12 +1,74 @@
 import socket
+import json
  
 host = "localhost"
 port = 8080
  
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.connect((host,port))
+
+class Handle():
+    def __init__(self, socket):
+        self.socket = socket
+
+    def login(self, data):
+        sData = {"type": "login", "username": data[1]}
+        jData = json.dumps(sData)
+        self.socket.send(jData.encode())
+
+    def logout(self, data):
+        sData = {"type": "logout"}
+        jData = json.dumps(sData)
+        self.socket.send(jData.encode())
+
+    def help(self, data):
+        print("login [username], logout [], help [], exit []")
+
+    def __main__(self, data):
+        type = data[0]
+        switch = {
+            "login": self.login,
+            "logout": self.logout,
+            "help": self.help
+        }
+        try:
+            return switch[type](data)
+        except Exception as e:
+            print(type)
+            print(data)
+            print(e)
+
+class Client():
+    def __main__(self):
+        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        try:
+            client_socket.connect((host,port))
+        except Exception as e:
+            print(e)
+        print("Connect to Server.\nInput format: Command Information. If need help, enter help as Command.")
+        handle = Handle(client_socket)
+        while True:
+            rawData = input()
+            data = rawData.split()
+            if(data[0]=="exit"):
+                break
+            else:
+                handle.__main__(data)
+            recvData = client_socket.recv(1024)
+            print(recvData.decode())
+
+        
+
+
+
+if __name__ == "__main__":
+    client=Client()
+    client.__main__()
+
+
+
+"""
 data = input("Input massage: ")
 client_socket.sendto(data.encode(),(host,port))
 data = client_socket.recv(1024)
 client_socket.close()
 print(data.decode())
+"""
--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 import socket
 import threading
+import json
 
-from requests import request
 
 
 host= 'localhost'
@@ -14,12 +14,60 @@ class User():
         self.csocket = socket
 
 class HandleRequest():
+    """
+    data = {
+        username:
+        type:
+        status:
+        info:
+    }
+    """
+    usernames={}
+
     def __init__(self, user):
         self.user = user
 
+    def deluser(user):
+        try:
+            HandleRequest.usernames.pop(user)
+        except Exception as e:
+            print(e)
+
+    def send2Me(self, data):
+        jData = json.dumps(data)
+        self.user.csocket.send(jData.encode())
+        print("send "+jData)
+
+    def login(self, data):
+        if self.user in HandleRequest.usernames.keys():
+            data['status'] = False
+            data['info'] = "You already login."
+        elif data['username'] in HandleRequest.usernames.values():
+            data['status'] = False
+            data['info'] = "This username is already in use."
+        else:
+            data['status'] = True
+            HandleRequest.usernames[self.user] = data['username']
+        self.send2Me(data)
+
+    def logout(self, data):
+        print(HandleRequest.usernames[self.user]+"logout.")
+        HandleRequest.deluser(self.user)
+
     def __main__(self, data):
-        print("recieve from {addr}".format(addr=self.user.caddress))
-        print(data.decode())
+        type = data['type']
+        switch = {
+            "login": self.login,
+            "logout": self.logout, 
+        }
+        #try:
+        return switch[type](data)
+        """except Exception as e:
+            print(e)
+            data['status'] = False
+            data['info'] = "Unkown error"
+            return data
+        """
 
 class ClientThread(threading.Thread):
     def __init__(self, user):
@@ -27,13 +75,19 @@ class ClientThread(threading.Thread):
         self.user = user
 
     def run(self):
-        try:
-            handle = HandleRequest(self.user)
+        #try:
+        handle = HandleRequest(self.user)
+        while True:
             request_data = self.user.csocket.recv(2048)
-            handle.__main__(request_data)
-        except Exception as e:
-            print("Disconnect...")
-            print(e)
+            data = json.loads(request_data.decode())
+            print("Receive "+request_data.decode())
+            if data['type'] == 'logout':
+                break
+            else:
+                handle.__main__(data)
+        #except Exception as e:
+        #    print("Disconnect...")
+        #    print(e)
         self.user.csocket.sendto("Reply from server.".encode(), (self.user.caddress))
         self.user.csocket.close()
 
