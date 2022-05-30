f3db55823044f4c563262f3ad9ce1901fe09b4c5
Xingjian <59945588+xingjianW@users.noreply.github.com>
2022-05-29 20:16:55 -0700

    完成了私聊功能
    顺便改了许多bug，现在程序应该能应对各种奇怪的输入。不过服务器暂时还是不能处理客户端的突然断开连接

--- client.py
+++ client.py
@@ -1,5 +1,6 @@
 import socket
 import json
+import threading
  
 host = "localhost"
 port = 8080
@@ -9,49 +10,116 @@ class Handle():
     def __init__(self, socket):
         self.socket = socket
 
-    def login(self, data):
-        sData = {"type": "login", "username": data[1]}
-        jData = json.dumps(sData)
-        self.socket.send(jData.encode())
-
-    def logout(self, data):
-        sData = {"type": "logout"}
-        jData = json.dumps(sData)
-        self.socket.send(jData.encode())
-
-    def help(self, data):
+    def help(self):
         print("****************************")
-        print(" Opreation Menu ")
+        print(" Menu Command ")
         print("****************************")      
         print("register USERNAME")
         print("login USERNAME")
-        print("logout USERNAME")
-        print("startshat USERNAME")
-        print("createchatroom ROOMNAME")
-        print("joinchatroom ROOMNAME")
-        print("userlist")
-        print("roomlist\n")
-        print("Type to Perform Operation: ")
+        print("logout")
+        print("listUsers")
+        print("chatWith USERNAME")
+        print("****************************")  
+        print("Room Command")
+        print("****************************")  
+        print(">> MSG")
+        return True
+
+    def sendType(self, data):
+        sData = {"type": data[0]}
+        jData = json.dumps(sData)
+        self.socket.send(jData.encode())
+        return True
+
+    def sendName(self, data):
+        sData = {"type": data[0], "username": data[1]}
+        jData = json.dumps(sData)
+        self.socket.send(jData.encode())
+        return True
+
+    def sendTo(self, data):
+        if len(data) == 3 and data[2] == "0":
+            sData = {"type": data[0], "to": data[1], "notChatWith": False}
+            jData = json.dumps(sData)
+            self.socket.send(jData.encode())
+            return True
+        else:
+            sData = {"type": data[0], "to": data[1]}
+            jData = json.dumps(sData)
+            self.socket.send(jData.encode())
+            return True
+
+    def sendMsg(self, data):
+        sData = {"type": data[0], "msg": data[1]}
+        jData = json.dumps(sData)
+        self.socket.send(jData.encode())
+        return True
 
     def __main__(self, data):
         type = data[0]
-        switch = {
-            "help": self.help,
-            #"register": self.register
-            "login": self.login,
-            "logout": self.logout,
-            #"startchat":self.startchat
-            #"createchatroom":self.createchatroom
-            #"joinchatroom":self.joinchatroom
-            #"userlist":self.userlist
-            #"roomlist":self.roomlist
-        }
+        """
+        register    reply
+        login   reply
+        logout  reply
+        listUsers   reply
+        chatWith    reply
+        >>  noreply
+        """
         try:
-            return switch[type](data)
+            if type == "help":
+                return self.help()
+            elif type == "register":
+                return self.sendName(data)
+            elif type == "login":
+                return self.sendName(data)
+            elif type == "logout":
+                self.sendType(data)
+                return False
+            elif type == "listUsers":
+                return self.sendType(data)
+            elif type == "chatWith":
+                return self.sendTo(data)
+            elif type == ">>":
+                return self.sendMsg(data)
+            else:
+                print("Incorrect command. If need help, enter help as Command.")
+                return False
+
         except Exception as e:
-            print(type)
+            print("Unkown error. If need help, enter help as Command.")
             print(data)
             print(e)
+            return False
+
+class listenThread(threading.Thread):
+    def __init__(self, csocket, handle):
+        threading.Thread.__init__(self)
+        self.sock = csocket
+        self.handle = handle
+
+    def run(self):
+        while True:
+            try:
+                recvData = self.sock.recv(1024)
+                data = json.loads(recvData.decode())
+                if data['type'] == "recieve":
+                    print(data['msg'])
+                elif data['type'] == "enterChat":
+                    rdata = ["chatWith", data['to'], "0"]
+                    self.handle.sendTo(rdata)
+                else:
+                    if data['status'] == True:
+                        print("Execution succeed. From server: "+repr(data['info']))
+                    elif data['status']  == False:
+                        print("Execution failed. From server: "+repr(data['info']))
+                    else:
+                        print("Holyshit..."+repr(data))
+                    if data['type'] == "logout":
+                        break
+
+            except Exception as e:
+                print("Disconnect...")
+                break
 
 class Client():
     def __main__(self):
@@ -62,15 +130,18 @@ class Client():
             print(e)
         print("Connect to Server.\nInput format: Command Information. If need help, enter help as Command.")
         handle = Handle(client_socket)
+        t = listenThread(client_socket, handle)
+        t.start()
         while True:
             rawData = input()
             data = rawData.split()
-            if(data[0]=="exit"):
+            if data[0] == "exit":
                 break
             else:
-                handle.__main__(data)
-            recvData = client_socket.recv(1024)
-            print(recvData.decode())
+                keepRun = handle.__main__(data)
+            if not keepRun:
+                return
+
 
 if __name__ == "__main__":
     client=Client()
--- server.py
+++ server.py
@@ -1,6 +1,8 @@
+from collections import UserList
 import socket
 import threading
 import json
+import time
 
 
 
@@ -21,10 +23,17 @@ class HandleRequest():
         info:
     }
     """
-    usernames={}
+    usernames = {}    #key is User object, value is username
+    userFile = open("AllUsernames.txt", "r")
+    userList = userFile.read()
+    userFile.close
 
     def __init__(self, user):
         self.user = user
+        self.reciverList = []
+
+    def getUser(self, nameList):
+        return [k for k, u in HandleRequest.usernames.items() if u in nameList]
 
     def deluser(user):
         try:
@@ -35,38 +44,146 @@ class HandleRequest():
     def send2Me(self, data):
         jData = json.dumps(data)
         self.user.csocket.send(jData.encode())
-        print("send "+jData)
+        print("Send "+jData)
+
+    def send2User(self, data, recv):
+        jData = json.dumps(data)
+        for r in recv:
+            r.csocket.send(jData.encode())
+        print("Send to "+ repr(self.reciverList)+". Msg: "+repr(jData))
+
+    def register(self, data):
+        userTxt=open("AllUsernames.txt", "a")
+        if data['username'] in HandleRequest.userList:
+            userTxt.close()
+            userFile = open("AllUsernames.txt", "r")
+            HandleRequest.userList = userFile.read().split("\n")
+            userFile.close
+            data['status'] = False
+            data['info'] = "This username is already in use. Please login."
+        else:
+            userTxt.write(data['username']+"\n")
+            userTxt.close()
+            userFile = open("AllUsernames.txt", "r")
+            HandleRequest.userList = userFile.read().split("\n")
+            userFile.close
+            data['status'] = True
+            data['info'] = "Successfully registered."
+        self.send2Me(data)
+        return True
 
     def login(self, data):
-        if self.user in HandleRequest.usernames.keys():
+        if data['username'] in HandleRequest.usernames.values():
             data['status'] = False
             data['info'] = "You already login."
-        elif data['username'] in HandleRequest.usernames.values():
+        elif data['username'] not in HandleRequest.userList:
             data['status'] = False
-            data['info'] = "This username is already in use."
+            data['info'] = "Please register first."
         else:
-            data['status'] = True
             HandleRequest.usernames[self.user] = data['username']
+            data['status'] = True
+            data['info'] = "Successfully login."
         self.send2Me(data)
+        return True
 
     def logout(self, data):
-        print(HandleRequest.usernames[self.user]+"logout.")
+        print(HandleRequest.usernames[self.user]+" logout.")
+        data['status'] = True
+        data['info'] = "Successfully logout."
+        self.send2Me(data)
         HandleRequest.deluser(self.user)
+        return False
+
+    def listUsers(self, data):
+        data['status'] = True
+        data['info'] = list(HandleRequest.usernames.values())
+        self.send2Me(data)
+        return True
+
+    def chatWith(self, data):
+        if HandleRequest.usernames[self.user] > data['to']:
+            fileName = HandleRequest.usernames[self.user] + "_" + data['to'] + ".txt"
+        else:
+            fileName = data['to'] + "_" + HandleRequest.usernames[self.user] + ".txt"
+        userFile = open(fileName, "a")
+        userFile.close()
+        userFile = open(fileName, "r")
+        msg = userFile.read()
+        userFile.close()
+        msg = msg.split('\n')
+        unreadMsg = []
+        unread = False
+        for line in msg:
+            if "unread" in line:
+                unread = True
+                newMsg = line.split("$#@")
+                newMsg = newMsg[0]+"  From: "+newMsg[1]+"    "+newMsg[2]
+                unreadMsg.append(newMsg)
+        if unread == True:
+            self.reciverList = []
+            self.reciverList.append(data['to'])
+            data['status'] = True
+            data['info'] = "Start to chat with "+data['to']
+            data['msg'] = unreadMsg
+            self.send2Me(data)
+            if "notChatWith" not in data.keys():
+                rdata = {'type': "enterChat", 'to': HandleRequest.usernames[self.user]}
+                jData = json.dumps(rdata)
+                recv = self.getUser(self.reciverList)
+                recv[0].csocket.send(jData.encode())
+
+        else:
+            self.reciverList = []
+            self.reciverList.append(data['to'])
+            data['status'] = True
+            data['info'] = "Start to chat with "+data['to']
+            self.send2Me(data)
+            if "notChatWith" not in data.keys():
+                rdata = {'type': "enterChat", 'to': HandleRequest.usernames[self.user]}
+                jData = json.dumps(rdata)
+                recv = self.getUser(self.reciverList)
+                recv[0].csocket.send(jData.encode())
+        return True
+
+    def sendMsg(self, data):
+        if len(self.reciverList) == 1:
+            if HandleRequest.usernames[self.user] > self.reciverList[0]:
+                fileName = HandleRequest.usernames[self.user] + "_" + self.reciverList[0] + ".txt"
+            else:
+                fileName = self.reciverList[0] + "_" + HandleRequest.usernames[self.user] + ".txt"
+            userFile = open(fileName, "a")
+            t = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+            # time $#@ from $#@ msg $#@ unread. But unread need ping().
+            formatMsg = t +"$#@"+ HandleRequest.usernames[self.user] +"$#@"+ data['msg'] +"\n"
+            userFile.write(formatMsg)
+            userFile.close()
+
+            recv = self.getUser(self.reciverList)
+            data['status'] = True
+            data['type'] = "recieve"
+            data['from'] = HandleRequest.usernames[self.user]
+            self.send2User(data, recv)
+        return True
+
 
     def __main__(self, data):
         type = data['type']
         switch = {
             "login": self.login,
             "logout": self.logout, 
+            "register": self.register,
+            "listUsers": self.listUsers,
+            "chatWith": self.chatWith,
+            ">>": self.sendMsg,
         }
-        #try:
-        return switch[type](data)
-        """except Exception as e:
+        try:
+            return switch[type](data)
+        except Exception as e:
             print(e)
             data['status'] = False
             data['info'] = "Unkown error"
-            return data
-        """
+            return self.send2Me(data)
+        
 
 class ClientThread(threading.Thread):
     def __init__(self, user):
@@ -74,21 +191,22 @@ class ClientThread(threading.Thread):
         self.user = user
 
     def run(self):
-        #try:
-        handle = HandleRequest(self.user)
-        while True:
-            request_data = self.user.csocket.recv(2048)
-            data = json.loads(request_data.decode())
-            print("Receive "+request_data.decode())
-            if data['type'] == 'logout':
-                break
-            else:
-                handle.__main__(data)
-        #except Exception as e:
-        #    print("Disconnect...")
-        #    print(e)
-        self.user.csocket.sendto("Reply from server.".encode(), (self.user.caddress))
-        self.user.csocket.close()
+        try:
+            handle = HandleRequest(self.user)
+            while True:
+                request_data = self.user.csocket.recv(2048)
+                data = json.loads(request_data.decode())
+                print("Receive "+request_data.decode())
+                keepRun = handle.__main__(data)
+                if not keepRun:
+                    break
+        except Exception as e:
+            print("Disconnect with error...")
+            print(e)
+        finally:
+            #print(repr(HandleRequest.usernames[self.user])+" logout.")
+            #HandleRequest.deluser(self.user)
+            self.user.csocket.close()
 
 
 class Server():
@@ -97,11 +215,20 @@ class Server():
         server_socket.bind((host,port))
         server_socket.listen(5)
 
+        threads = []
         while True:
-            client_socket, client_address = server_socket.accept()
-            user = User(client_address, client_socket)
-            t = ClientThread(user)
-            t.start()
+            try:
+                client_socket, client_address = server_socket.accept()
+                user = User(client_address, client_socket)
+                t = ClientThread(user)
+                threads+=[t]
+                t.start()
+            except KeyboardInterrupt:
+                print('KeyboardInterrupt:')
+                for th in threads:
+                    th.stop()
+                break
+        server_socket.close()
 
 if __name__ == "__main__":
     server=Server()
